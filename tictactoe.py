257694cecaa5874caa8d0e42e178e7736a4f8983
xAlanna <axs204@nyu.edu>
2017-07-25 07:08:08 -0400

    Add files via upload

---
+++ tictactoe.py
@@ -0,0 +1,133 @@
+# Tic Tac Toe
+
+import pygame, sys, random
+from pygame.locals import *
+
+# Create the constants (go ahead and experiment with different values)
+BOARDWIDTH = 3  # number of columns in the board
+BOARDHEIGHT = 3 # number of rows in the board
+TILESIZE = 80
+WINDOWWIDTH = 640
+WINDOWHEIGHT = 480
+FPS = 30
+
+#                 R    G    B
+BLACK =         (  0,   0,   0)
+WHITE =         (255, 255, 255)
+
+
+BGCOLOR = WHITE
+TILECOLOR = BLACK
+TEXTCOLOR = WHITE
+BORDERCOLOR = BLACK
+BASICFONTSIZE = 20
+
+BUTTONCOLOR = WHITE
+BUTTONTEXTCOLOR = BLACK
+MESSAGECOLOR = WHITE
+
+XMARGIN = int((WINDOWWIDTH - (TILESIZE * BOARDWIDTH + (BOARDWIDTH - 1))) / 2)
+YMARGIN = int((WINDOWHEIGHT - (TILESIZE * BOARDHEIGHT + (BOARDHEIGHT - 1))) / 2)
+
+
+def main():
+    global FPSCLOCK, DISPLAYSURF, BASICFONT, NEW_SURF, NEW_RECT
+
+    pygame.init()
+    FPSCLOCK = pygame.time.Clock()
+    DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+    pygame.display.set_caption('Tic Tac Toe')
+    BASICFONT = pygame.font.Font('freesansbold.ttf', BASICFONTSIZE)
+
+    # Store the option buttons and their rectangles in OPTIONS.
+    NEW_SURF,   NEW_RECT   = makeText('New Game', TEXTCOLOR, TILECOLOR, WINDOWWIDTH - 120, WINDOWHEIGHT - 60)
+
+    currentBoard = getStartingBoard()
+
+    while True: # main game loop
+	drawBoard(currentBoard)
+        checkForQuit()
+        pygame.display.update()
+        FPSCLOCK.tick(FPS)
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def checkForQuit():
+    for event in pygame.event.get(QUIT): # get all the QUIT events
+        terminate() # terminate if any QUIT events are present
+    for event in pygame.event.get(KEYUP): # get all the KEYUP events
+        if event.key == K_ESCAPE:
+            terminate() # terminate if the KEYUP event was for the Esc key
+        pygame.event.post(event) # put the other KEYUP event objects back
+
+
+def getStartingBoard():
+    # Return a board data structure with tiles.
+    counter = 1
+    board = []
+    for x in range(BOARDWIDTH):
+        column = []
+        for y in range(BOARDHEIGHT):
+            column.append(counter)
+            counter += BOARDWIDTH
+        board.append(column)
+        counter -= BOARDWIDTH * (BOARDHEIGHT - 1) + BOARDWIDTH - 1
+
+    return board
+
+
+
+def getLeftTopOfTile(tileX, tileY):
+    left = XMARGIN + (tileX * TILESIZE) + (tileX - 1)
+    top = YMARGIN + (tileY * TILESIZE) + (tileY - 1)
+    return (left, top)
+
+
+def getSpotClicked(board, x, y):
+    # from the x & y pixel coordinates, get the x & y board coordinates
+    for tileX in range(len(board)):
+        for tileY in range(len(board[0])):
+            left, top = getLeftTopOfTile(tileX, tileY)
+            tileRect = pygame.Rect(left, top, TILESIZE, TILESIZE)
+            if tileRect.collidepoint(x, y):
+                return (tileX, tileY)
+    return (None, None)
+
+
+def drawTile(tilex, tiley, number, adjx=0, adjy=0):
+    # draw a tile at board coordinates tilex and tiley, optionally a few
+    # pixels over (determined by adjx and adjy)
+    left, top = getLeftTopOfTile(tilex, tiley)
+    pygame.draw.rect(DISPLAYSURF, TILECOLOR, (left + adjx, top + adjy, TILESIZE, TILESIZE))
+
+
+def makeText(text, color, bgcolor, top, left):
+    # create the Surface and Rect objects for some text.
+    textSurf = BASICFONT.render(text, True, color, bgcolor)
+    textRect = textSurf.get_rect()
+    textRect.topleft = (top, left)
+    return (textSurf, textRect)
+
+
+def drawBoard(board):
+    DISPLAYSURF.fill(BGCOLOR)
+
+    for tilex in range(len(board)):
+        for tiley in range(len(board[0])):
+            if board[tilex][tiley]:
+                drawTile(tilex, tiley, board[tilex][tiley])
+
+    left, top = getLeftTopOfTile(0, 0)
+    width = BOARDWIDTH * TILESIZE
+    height = BOARDHEIGHT * TILESIZE
+    pygame.draw.rect(DISPLAYSURF, BORDERCOLOR, (left - 5, top - 5, width + 11, height + 11), 4)
+
+    DISPLAYSURF.blit(NEW_SURF, NEW_RECT)
+
+
+if __name__ == '__main__':
+    main()
